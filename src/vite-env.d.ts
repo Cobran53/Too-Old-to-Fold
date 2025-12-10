91591111f623686e0b4865d77b607455125d5bcf
Nolann Boyere <nolann.boyere@etu.univ-nantes.fr>
2025-12-10 18:03:22 +0100

    added yaml support

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+declare module '*.yaml' {
+  const data: any; // Or use a more specific type if you have one defined
+  export default data;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import yaml from '@rollup/plugin-yaml'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    yaml()
   ],
   test: {
     globals: true,
